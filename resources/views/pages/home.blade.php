3363e672d36f6b7b58da71e88ea3bf61eab69d7e
yuxxeun <itsyuxxeun@gmail.com>
2022-10-21 06:56:50 +0700

    Add Laravel debuger

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base', ['title' => 'gatekeeper'])
 @section('content')
 <section class="pt-20">
-<h1 class="text-5xl font-bold tracking-widest font-serif italic">GATEKEEPER</h1>
+<h1 class="text-5xl font-basement font-bold tracking-widest italic">GATEKEEPER</h1>
 <section>
 @endsection
