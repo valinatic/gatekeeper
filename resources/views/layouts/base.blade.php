e7a672a2d287c20e43dba081e6542a29ca2323d0
yuxxeun <itsyuxxeun@gmail.com>
2022-10-20 21:29:24 +0700

    Add layout to wrap view

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>{{ $title === 'gatekeeper' ? $title : 'gatekeeper | '.$title }}</title>
+    </head>
+    <body class="my-5 px-5 text-center">
+        @include('components.nav')
+        <div class="my-10">
+            @yield('content')
+        </div>
+        @include('components.foot')
+    </body>
+</html>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base', ['title' => 'About Us'])
+@section('content')
+
+<section>
+this is About Us, inside valinatic laboratory
+<section>
+@endsection
