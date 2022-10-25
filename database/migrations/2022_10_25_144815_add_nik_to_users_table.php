6d958b6d33cc2161b87546446acd0a50e4f371bd
yuxxeun <itsyuxxeun@gmail.com>
2022-10-25 22:47:31 +0700

    Adding tailwind-prettier package

--- database/migrations/2022_10_25_144815_add_nik_to_users_table.php
+++ database/migrations/2022_10_25_144815_add_nik_to_users_table.php
@@ -10,7 +10,7 @@ class AddNikToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nik');
+            $table->string('nik')->uniqid();
         });
     }
 
