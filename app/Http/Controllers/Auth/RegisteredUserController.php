0af6450d8ab6516630e04fc2f66524cb100669a2
yuxxeun <itsyuxxeun@gmail.com>
2022-10-25 23:12:55 +0700

    Add auth with NIK

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,7 +34,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'nik' => ['required', 'string', 'max:255'],
+            'nik' => ['required', 'string', 'max:16'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
