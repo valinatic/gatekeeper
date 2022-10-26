af1703533e3c0048f9d6e70fb360eca5ee4f3495
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-10-26 19:27:51 +0700

    add candidate card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,12 +6,101 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="grid grid-cols-2 gap-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white space-y-4">
+                        <img src="https://loremflickr.com/640/480/people" class="rounded-lg" alt="">
+                        <h3 class="text-2xl text-center font-bold tracking-tight">Evan You</h3>
+
+                        <div id="accordion-collapse-1" data-accordion="collapse">
+                            <h2 id="visi-accordion-collapse-1-heading-3">
+                              <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#visi-accordion-collapse-1-body-3" aria-expanded="false" aria-controls="visi-accordion-collapse-1-body-3">
+                                <span>Visi</span>
+                                <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                              </button>
+                            </h2>
+                            <div id="visi-accordion-collapse-1-body-3" class="hidden" aria-labelledby="visi-accordion-collapse-1-heading-3">
+                              <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700">
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                  <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                  <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                </ul>
+                              </div>
+                            </div>
+                          <h2 id="misi-accordion-collapse-1-heading-3">
+                            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#misi-accordion-collapse-1-body-3" aria-expanded="false" aria-controls="misi-accordion-collapse-1-body-3">
+                              <span>Misi</span>
+                              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                          </h2>
+                          <div id="misi-accordion-collapse-1-body-3" class="hidden" aria-labelledby="misi-accordion-collapse-1-heading-3">
+                            <div class="p-5 font-light border border-t-0 border-gray-200 dark:border-gray-700">
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                              <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Vote</button>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white space-y-4">
+                        <img src="https://loremflickr.com/640/480/people" class="rounded-lg" alt="">
+                        <h3 class="text-2xl text-center font-bold tracking-tight">Lukas Christensen</h3>
+
+                        <div id="2-accordion-collapse-1" data-accordion="collapse">
+                            <h2 id="visi-2-accordion-collapse-1-heading-3">
+                              <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#visi-2-accordion-collapse-1-body-3" aria-expanded="false" aria-controls="visi-2-accordion-collapse-1-body-3">
+                                <span>Visi</span>
+                                <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                              </button>
+                            </h2>
+                            <div id="visi-2-accordion-collapse-1-body-3" class="hidden" aria-labelledby="visi-2-accordion-collapse-1-heading-3">
+                              <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700">
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                                <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                  <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                  <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                                </ul>
+                              </div>
+                            </div>
+                          <h2 id="misi-2-accordion-collapse-1-heading-3">
+                            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#misi-2-accordion-collapse-1-body-3" aria-expanded="false" aria-controls="misi-2-accordion-collapse-1-body-3">
+                              <span>Misi</span>
+                              <svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </button>
+                          </h2>
+                          <div id="misi-2-accordion-collapse-1-body-3" class="hidden" aria-labelledby="misi-2-accordion-collapse-1-heading-3">
+                            <div class="p-5 font-light border border-t-0 border-gray-200 dark:border-gray-700">
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">The main difference is that the core components from Flowbite are open source under the MIT license, whereas Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone components, whereas Tailwind UI offers sections of pages.</p>
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no technical reason stopping you from using the best of two worlds.</p>
+                              <p class="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
+                              <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
+                                <li><a href="https://flowbite.com/pro/" class="text-blue-600 dark:text-blue-500 hover:underline">Flowbite Pro</a></li>
+                                <li><a href="https://tailwindui.com/" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Tailwind UI</a></li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Vote</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
 </x-app-layout>
